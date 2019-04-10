986f59ae18125898d7058ff2e6d35f1db6164205
theWorthyG <wdg2001@wildcats.unh.edu>
2019-04-10 16:59:59 -0400

    First push of Burger app, all features

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+-- create the database
+CREATE DATABASE burgers_db;
+USE `burgers_db`;
+
+-- create table
+CREATE TABLE `burgers`
+(
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`burger_name` VARCHAR(255) NOT NULL,
+	`devoured` BOOLEAN DEFAULT false,
+	`date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- select burgers_db
+USE `burgers_db`;
+
+-- insert burgers
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Beef', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Salmon', false, CURRENT_TIMESTAMP);
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('Bacon', false, CURRENT_TIMESTAMP);
